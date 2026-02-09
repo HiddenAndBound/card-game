cd39410373977be80fed3d41ce2c33f92f575b10
HiddenAndBound <ritwiksen9090@gmail.com>
2026-02-09 22:26:51 +0530

    initial commit

--- src/state.rs
+++ src/state.rs
@@ -1,1 +1,27 @@
+use bevy::ecs::schedule::States;
+
 // Game state definitions and transitions.
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default, States)]
+pub enum GameState {
+    #[default]
+    Menu,
+    Pause,
+    Run,
+    Combat,
+    Victory,
+    Defeat,
+}
+
+#[cfg(test)]
+mod tests {
+    use bevy::{DefaultPlugins, app::App};
+
+    use crate::state::GameState;
+
+    #[test]
+    fn test_init() {
+        let mut app = App::new();
+        let app = app.init_state::<GameState>();
+        app.add_plugins(DefaultPlugins).run();
+    }
+}
